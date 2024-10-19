2deb0a03b6d200ad3021388d91276c758e37a28e
trung11012002 <vuquangtrung098765@gmail.com>
2024-10-19 14:00:13 +0700

    Restore web-app from main

---
+++ web-app/src/components/FormLogin/index.tsx
@@ -0,0 +1,110 @@
+import React, { useContext, useState } from 'react';
+import './Form.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../../apis/auth';
+
+import { AuthContextProvider } from '../../contexts/AuthContext';
+import { MessageContextProvider } from '../../contexts/MessageContext';
+
+interface IFormLogin {
+    username: string | undefined
+    password: string | undefined
+}
+
+const Form = () => {
+    const auth = useContext(AuthContextProvider);
+    const message = useContext(MessageContextProvider);
+    const setUserState = auth?.setUserState
+    const success = message?.success
+    const error = message?.error
+
+    const [data, setData] = useState<IFormLogin>({
+        username: undefined,
+        password: undefined
+    })
+
+    const navigate = useNavigate()
+    const submitFormLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const res = await login(data)
+        console.log(res)
+        // console.log(res?.data)
+        if (res?.code === 1000) {
+            setUserState?.({
+                isLogin: true,
+                user: res.data,
+                tokenAccess: res.data.tokenAccess,
+                tokenRefresh: res.data.tokenRefresh
+
+            })
+            localStorage.setItem('tokenAccess', res.data.tokenAccess)
+            localStorage.setItem('tokenRefresh', res.data.tokenRefresh)
+            success("Đăng nhập thành công")
+            res.data.role.name === "SUPER_ADMIN" ?
+                navigate('/super-admin/theater-list')
+                :
+                res.data.role.name === "ADMIN" ?
+                    navigate('/admin/movie-schedule')
+                    :
+                    navigate('/')
+        }else{
+            error(res?.data.msg)
+        }
+    };
+
+    const navigateHome = async (e: any) => {
+        e.preventDefault();
+        navigate('/')
+    };
+
+    const changeInfor = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div className='Form'>
+            <div>
+
+                <div className='login-form'>
+                    <h1 onClick={navigateHome}>FILM BOOKING</h1>
+                    <form onSubmit={submitFormLogin}>
+                        <div>
+                            <label htmlFor="username">Tên đăng nhập:</label>
+                            <input
+                                id='username'
+                                type="username"
+                                name='username'
+                                placeholder='username'
+                                onChange={changeInfor}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Mật khẩu:</label>
+                            <input
+                                id='password'
+                                type="password"
+                                name='password'
+                                placeholder='******'
+                                onChange={changeInfor}
+                            />
+                        </div>
+                        <span onClick={() => navigate("/forgot-password")}>Quên mật khẩu???</span>
+                        <button>Đăng nhập</button>
+                    </form>
+
+                    <p>Bạn chưa có tài khoản <Link to='/register'>Đăng Ký</Link></p>
+                </div>
+
+                <div className='img-loginform'>
+                    <img src={"https://kaliforms.com/wp-content/uploads/2021/04/movie-ticket-booking-form-scaled.jpg"} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Form
