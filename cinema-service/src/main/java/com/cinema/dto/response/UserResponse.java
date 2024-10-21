26bfb50671411776be6eb04cdb4a40a8ad6728d6
trung11012002 <vuquangtrung098765@gmail.com>
2024-10-21 23:30:25 +0700

    update cinema service userresponse

---
+++ cinema-service/src/main/java/com/cinema/dto/response/UserResponse.java
@@ -0,0 +1,44 @@
+package com.cinema.dto.response;
+
+import com.cinema.entity.Cinema;
+import com.cinema.entity.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Integer id;
+    private String username;
+
+    private String password;
+
+    private String fullname;
+
+    private String dateOfBirth;
+
+    private String address;
+
+    private String email;
+
+    private String phone;
+
+    private boolean isBlocked = false;
+
+    @JsonIgnore
+    private Role role;
+
+    @JsonIgnore
+    private Cinema managedCinema;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
