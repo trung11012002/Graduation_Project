a1aac00870d332a4488018f2e8fb84c58f597e79
chinhv <vuducchinh2002@gmail.com>
2024-10-21 16:36:29 +0700

    update code

--- review-service/src/main/java/com/example/reviewservice/controller/RatingController.java
+++ review-service/src/main/java/com/example/reviewservice/controller/RatingController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 public class RatingController {
 
